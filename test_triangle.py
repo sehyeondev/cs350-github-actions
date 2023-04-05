fc74cc2991c87a0c600c046c4299da6c6f952956
Sehyeon Kim <sue2368@gmail.com>
2023-04-05 14:16:06 +0900

    fix bug in testcase

--- test_triangle.py
+++ test_triangle.py
@@ -2,6 +2,6 @@ from triangle import triangle
 def test_invalid1():
     assert triangle(-1, 0, 1) == -1
 def test_invalid2():
-    assert triangle(2, 2, 1) == -1
+    assert triangle(2, 2, 1) == 1
 def test_equilateral():
     assert triangle(3, 3, 3) == 1
